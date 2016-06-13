95d10e46e33b3c2355077d42f2f8a49263b7a8ee
Igor Balos <ibalosh@gmail.com>
2016-06-13 15:12:33 +0200

    updated hooks so it support adding properties to them

--- sinhook.rb
+++ sinhook.rb
@@ -21,6 +21,82 @@ class SinHook < Sinatra::Base
 
   end
 
+  class Actions
+
+    attr_reader :hook_id
+
+    def initialize
+
+      @actions = {}
+
+    end
+
+    def add(action, hook_id)
+
+      @actions[hook_id] = [] if @actions[hook_id].nil?
+      @actions[hook_id] << action
+
+    end
+
+    def delete(action, hook_id)
+
+      return if @actions[hook_id].nil?
+      @actions[hook_id].delete action
+
+    end
+
+    def clear(hook_id)
+
+      @actions[hook_id] = nil
+
+    end
+
+    def execute(hook_id)
+
+      return if @actions[hook_id].nil?
+      @actions[hook_id].each { |action| action.execute }
+
+    end
+
+  end
+
+  class Action
+
+
+  end
+
+  class Delay < Action
+
+    def initialize(seconds)
+
+      @seconds = seconds
+
+    end
+
+    def execute
+
+      sleep @seconds
+
+    end
+
+  end
+
+  class Error < Action
+
+    def initialize(status_code)
+
+      @status_code = status_code
+
+    end
+
+    def execute
+
+      halt @status_code
+
+    end
+
+  end
+
   configure do
 
     set :bind, '0.0.0.0'
@@ -30,6 +106,7 @@ class SinHook < Sinatra::Base
 
     set :hooks, Hooks.new(settings.hooks_to_store_count)
     set :response, Response.new
+    set :hook_actions, Actions.new
     set :maximum_hook_delay, 3600
 
   end
@@ -93,6 +170,7 @@ class SinHook < Sinatra::Base
 
     hook_id = params[:hook_id]
     hook_exists?(hook_id)
+    settings.hook_actions.execute(hook_id)
     settings.hooks.read_data(hook_id)
 
   end
@@ -110,7 +188,23 @@ class SinHook < Sinatra::Base
   # web hook will return status code :status_code
   put "/hook/:hook_id/break/:status_code" do
 
-    #if settings.hooks.is_available?(params[:hook_id])
+    hook_id = params[:hook_id]
+    hook_exists?(hook_id)
+    settings.hook_actions.add(Error.new(params[:status_code]), hook_id)
+    settings.response.message(:success, "Status code [#{params[:status_code]}] set.")
+
+  end
+
+  put "/hook/:hook_id/delayed/:seconds" do
+
+    hook_id = params[:hook_id]
+    hook_exists?(hook_id)
+
+    seconds = params[:seconds].to_i
+    seconds = (seconds > 0 && seconds < settings.maximum_hook_delay)? seconds : 0
+
+    settings.hook_actions.add(Delay.new(seconds), hook_id)
+    settings.response.message(:success, "Delayed response for #{seconds} seconds.")
 
   end
 
@@ -118,7 +212,9 @@ class SinHook < Sinatra::Base
   # web hook will return status code 200 from now on
   put "/hook/:hook_id/fix" do
 
-    #if settings.hooks.is_available?(params[:hook_id])
+    hook_id = params[:hook_id]
+    hook_exists?(hook_id)
+    settings.hook_actions.clear(hook_id)
 
   end
 
@@ -130,7 +226,7 @@ class SinHook < Sinatra::Base
       seconds = (seconds > 0 && seconds < settings.maximum_hook_delay)? seconds : 0
 
       sleep seconds
-      settings.response.message(:success, "Returned status: 200. Delayed response for #{seconds}")
+      settings.response.message(:success, "Delayed response for #{seconds} seconds.")
 
     end
 
